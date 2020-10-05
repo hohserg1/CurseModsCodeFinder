16b65aef8bc02b809bce457688826bc6450eecc3
hohserg1 <35610168+hohserg1@users.noreply.github.com>
2020-10-05 12:31:19 +0300

    Update ProjectsLinksDataminer.py
    CF fix

--- ProjectsLinksDataminer.py
+++ ProjectsLinksDataminer.py
@@ -1,40 +1,57 @@
 import requests
+import cloudscraper
 from bs4 import BeautifulSoup
+import time
+
+scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
+# Or: scraper = cloudscraper.CloudScraper()  # CloudScraper inherits from requests.Session
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 }
 
-loadPage = lambda url: requests.get(url, headers=headers).text
+loadPage = lambda url: scraper.get(url).text
 
-# for 1.13
-url = 'https://minecraft.curseforge.com/mc-mods?filter-game-version=2020709689:7132&filter-sort=4&page=%d'
-page_count = 15
+# for 1.12
+url = 'https://www.curseforge.com/minecraft/mc-mods?filter-game-version=2020709689:6756&filter-sort=4&page=%d'
+page_start = 1
+page_count = 432
 
-pages = [loadPage(url % page) for page in range(1, page_count)]
+pages = [loadPage(url % page) for page in range(page_start, page_count)]
 
 
-def getProjectsLinks(page):
-    text = pages[page - 1]
+def getProjectsLinks(pageNum):
+    print("parsing list page "+str(pageNum))
+    text = pages[pageNum - page_start]
     soup = BeautifulSoup(text, features='html.parser')
 
-    return list(map(lambda link: "https://minecraft.curseforge.com" + link.get('href'),
-                    soup.find_all('a', {'class': 'e-avatar64'})))
+    return list(filter(lambda e: "/mc-mods/" in e, list(map(lambda link: "https://www.curseforge.com" + str(link.get('href')),
+                    soup.find_all('a', {'class': 'my-auto'})))))
 
 
 flatten = lambda l: [item for sublist in l for item in sublist]
 
-projects = flatten(list(map(getProjectsLinks, range(1, page_count))))
+projects = flatten(list(map(getProjectsLinks, range(page_start,page_count))))
 
 
 def getSourceLink(project):
+    #print("parsing mod page "+project)
+    time.sleep(0.1)
     text = loadPage(project)
     soup = BeautifulSoup(text, features='html.parser')
     return list(map(lambda e: e.get("href"),
-                    list(filter(lambda e: "Source" in e.text, soup.find_all('a', {'class': 'external-link'})))))
+                    list(filter(lambda e: "Source" in e.text, soup.find_all('a', {'class': 'text-gray-500 hover:no-underline'})))))
 
 
 sourceLinks = flatten(list(map(getSourceLink, projects)))
 
 # use in finder
-print(sourceLinks)
+# print(*sourceLinks, sep="\n")
+print("source links combined!")
+
+github_pages = open("./github-pages.txt", "w")
+for e in sourceLinks:
+    github_pages.write(e+"\n")
+github_pages.close()
+
+
